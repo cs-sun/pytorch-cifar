c6bd3aa80b7d00eeb89f9e637171cbf11cddc5ed
cs-sun <me@cssun.xyz>
2020-01-08 18:47:20 +0800

    GPU Limits

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import argparse
 from models import *
 from utils import progress_bar
 
-
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,3'
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
